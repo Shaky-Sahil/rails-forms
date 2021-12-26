6d117aee0a8838b74c9fbdb67f649a2e9dbe9604
shaky-sahil <sahilpk81@gmail.com>
2021-12-26 12:24:35 +0530

    added validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true, length: { maximum: 15 }
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 6 }
 end
